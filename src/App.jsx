f0a4068866c1b7113d52b730c038bc0256f13060
paulina-pacheco-o <paulimichi05@gmail.com>
2025-09-02 15:42:40 +0200

    card

--- src/App.jsx
+++ src/App.jsx
@@ -16,8 +16,26 @@ function App() {
       <div className="container">
         <div className="row">
           <div className="col-12">
-            <button className="btn btn-primary" onClick={fetchActors}>Carica lista</button>
+            <h1>Lista attori</h1>
           </div>
+          {actors.map((actor) => {
+            return (
+              <div className="col-12" key={actor.id}>
+                <div className="card">
+                  <img src={actor.image} />
+                  <div>
+                    <h4>{actor.name}</h4>
+                    <ul>
+                      <li>{actor.birth_year}-{actor.death_year}</li>
+                      <li>{actor.nationality}</li>
+                      <li>{actor.biography}</li>
+                      <li>Know for: {actor.know_for}</li>
+                      <li>{actor.awards}</li>
+                    </ul>
+                  </div>
+                </div>
+              </div>)
+          })}
         </div>
       </div>
     </>
